2f867a48e0fe5cbcca1da4a784deed030aac37e3
leifniem <leif.niem@gmail.com>
2016-12-22 13:11:39 +0100

    package structure

---
+++ src/controller/MainViewController.java
@@ -0,0 +1,9 @@
+package controller;
+
+public class MainViewController {
+	
+	public MainViewController(){
+		
+	}
+	
+}
---
+++ src/launcher/Launcher.java
@@ -0,0 +1,28 @@
+package launcher;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import views.*;
+
+public class Launcher extends Application {
+	
+	public static void main(String[] args){
+        launch(args);
+    }
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("/views/MainView.fxml"));
+		
+		//Alternative:
+		//FXMLLoader loader = new FXMLLoader();
+		//Parent root2 = loader.load(new FileInputStream(filepath + "/HelloWorldView.fxml"));
+
+		Scene scene = new Scene(root, 300, 275);
+		stage.setTitle("Loop-Bot 3000");
+		stage.setScene(scene);
+		stage.show();
+	}
+}

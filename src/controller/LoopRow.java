a752c46287dfcd1ce1f0e5d65c48c73eb9161871
draxes <michalanvers@freenet.de>
2016-12-30 13:28:23 +0100

    implemented controls in the LoopRow

--- src/controller/LoopRow.java
+++ src/controller/LoopRow.java
@@ -1,19 +1,33 @@
 package controller;
 
 import java.io.IOException;
+
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.*;
 import javafx.scene.layout.*;
 
-public class LoopRow extends HBox{
-	public LoopRow(){
+public class LoopRow extends HBox {
+	@FXML
+	private Label nameLabel;
+	@FXML
+	private Button chooseFileButton;
+	@FXML
+	private Button recordButton;
+	@FXML
+	private Button soloButton;
+	@FXML
+	private Button muteButton;
+	
+	public LoopRow() {
 		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/views/LoopRowView.fxml"));
 		fxmlLoader.setRoot(this);
 		fxmlLoader.setController(this);
-		
+
 		try {
-            fxmlLoader.load();
-        } catch (IOException exception) {
-            throw new RuntimeException(exception);
-        }
+			fxmlLoader.load();
+		} catch (IOException exception) {
+			throw new RuntimeException(exception);
+		}
 	}
 }

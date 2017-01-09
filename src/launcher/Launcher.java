9d3244bc21fb01eb63eddd5d1ff6a4a02ffeee86
draxes <michalanvers@freenet.de>
2017-01-09 12:29:45 +0100

    close program

--- src/launcher/Launcher.java
+++ src/launcher/Launcher.java
@@ -1,9 +1,11 @@
 package launcher;
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class Launcher extends Application {
 	
@@ -16,8 +18,18 @@ public class Launcher extends Application {
 		VBox root = FXMLLoader.load(getClass().getResource("/views/MainView.fxml"));	
 
 		Scene scene = new Scene(root, 1000, 500);
+		addOnCloseRequestHandlerToPrimaryStage(stage);
 		stage.setTitle("Loop-Bot 3000");
 		stage.setScene(scene);
-		stage.show();
+		stage.show();	
+	}
+	
+	private void addOnCloseRequestHandlerToPrimaryStage(Stage primaryStage) {
+		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+			@Override
+			public void handle(WindowEvent e) {
+				System.exit(0);
+			}
+		});
 	}
 }

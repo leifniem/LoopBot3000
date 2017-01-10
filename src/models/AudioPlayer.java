0726a3a203ecd8187da2f9aea8b82e33c4ea7228
draxes <michalanvers@freenet.de>
2017-01-10 15:33:22 +0100

    Auskommentierter Code im AudioPlayer

--- src/models/AudioPlayer.java
+++ src/models/AudioPlayer.java
@@ -1,30 +1,65 @@
 package models;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.util.LinkedList;
 import java.util.List;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
 public class AudioPlayer {
 	private final static int SOUND_LIMIT = 16;
-	
+
 	private List<MediaPlayer> mediaPlayers = new LinkedList<MediaPlayer>();
-	
+
 	public void playShortSound(String filename){
+		/*
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					Clip clip = AudioSystem.getClip();
+					//InputStream inputStream = new FileInputStream(filename);
+					File file = new File(filename);
+					AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(file);
+					clip.open(audioInputStream);
+					clip.start();
+				} catch (LineUnavailableException e) {
+					e.printStackTrace();
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				} catch (UnsupportedAudioFileException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}).start();
+		*/
+
 		MediaPlayer mediaPlayer = getNewMediaPlayerFor(filename);
 		mediaPlayer.play();
 	}
-	
-	public void playLongSound(String filename){
-		if(mediaPlayers.size() < SOUND_LIMIT){
+
+	public void playLongSound(String filename) {
+		if (mediaPlayers.size() < SOUND_LIMIT) {
 			MediaPlayer mediaPlayer = getNewMediaPlayerFor(filename);
 			mediaPlayers.add(mediaPlayer);
 			mediaPlayer.play();
 			mediaPlayer.setOnEndOfMedia(createRemoveMediaPlayerRunnable(mediaPlayer));
-			mediaPlayer.setOnStopped(createRemoveMediaPlayerRunnable(mediaPlayer));	
+			mediaPlayer.setOnStopped(createRemoveMediaPlayerRunnable(mediaPlayer));
 		}
 	}
 
@@ -33,21 +68,21 @@ public class AudioPlayer {
 		MediaPlayer mediaPlayer = new MediaPlayer(media);
 		return mediaPlayer;
 	}
-	
-	public void stopAll(){
-		for(MediaPlayer mp : mediaPlayers){
+
+	public void stopAll() {
+		for (MediaPlayer mp : mediaPlayers) {
 			mp.stop();
 		}
 	}
-	
-	private Runnable createRemoveMediaPlayerRunnable(MediaPlayer mediaPlayer){
-		Runnable result = new Runnable(){
+
+	private Runnable createRemoveMediaPlayerRunnable(MediaPlayer mediaPlayer) {
+		Runnable result = new Runnable() {
 			@Override
 			public void run() {
-				mediaPlayers.remove(mediaPlayer);	
+				mediaPlayers.remove(mediaPlayer);
 			}
 		};
-		
+
 		return result;
 	}
 }

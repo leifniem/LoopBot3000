00bbaccf3b36205f711ad9cb249607d3911ef4d3
draxes <michalanvers@freenet.de>
2017-01-11 22:34:20 +0100

    Doppelter Start verhindern

--- src/launcher/Launcher.java
+++ src/launcher/Launcher.java
@@ -1,4 +1,5 @@
 package launcher;
+
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
@@ -8,24 +9,38 @@ import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
 public class Launcher extends Application {
-	
-	public static void main(String[] args){
-        launch(args);
-    }
+
+	public static void main(String[] args) {
+		endProgramIfAlreadyStarted();
+
+		launch(args);
+	}
+
+	private static void endProgramIfAlreadyStarted() {
+		try {
+			java.net.ServerSocket ss = new java.net.ServerSocket(666);
+		} catch (java.net.BindException ex) {
+			System.out.println("Programm läuft bereits.");
+			System.exit(1);
+		} catch (java.io.IOException ex) {
+			ex.printStackTrace();
+			System.exit(1);
+		}
+	}
 
 	@Override
 	public void start(Stage stage) throws Exception {
-		VBox root = FXMLLoader.load(getClass().getResource("/views/MainView.fxml"));	
+		VBox root = FXMLLoader.load(getClass().getResource("/views/MainView.fxml"));
 
 		Scene scene = new Scene(root, 1000, 500);
-		//hier könnte der InputManager initialisiert werden!
-		
+		// hier könnte der InputManager initialisiert werden!
+
 		addOnCloseRequestHandlerToPrimaryStage(stage);
 		stage.setTitle("Loop-Bot 3000");
 		stage.setScene(scene);
-		stage.show();	
+		stage.show();
 	}
-	
+
 	private void addOnCloseRequestHandlerToPrimaryStage(Stage primaryStage) {
 		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
 			@Override
